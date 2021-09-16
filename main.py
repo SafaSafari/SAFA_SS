54eb607df4fc9feee61ca4b5d1a3f756fefaf60b
Safa <safasafari.ss@gmail.com>
2021-09-16 16:02:32 +0430

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def get(proxy, send=False):
 async def send(send, proxy):
     if not sys.argv[2]:
         return
-    proxy = 'socks5://{}'.format(proxy)
+    proxy = 'socks5h://{}'.format(proxy)
     connector = ProxyConnector.from_url(proxy)
     async with ClientSession(connector=connector) as session:
         async with session.post('https://api.telegram.org/bot{}/sendMessage'.format(sys.argv[2]), data={"chat_id": "@SafaProxy", "text": send, "parse_mode": "markdown", "disable_web_page_preview": True}, ssl=False):
