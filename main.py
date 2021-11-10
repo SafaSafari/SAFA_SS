b1042fca77a4687a955b99e1fb79e3265abb9320
Safa <safasafari.ss@gmail.com>
2021-11-11 02:49:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def decode_base64(data, altchars=b'+/'):
     :returns: The decoded byte string.
 
     """
-    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
+    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data.encode('utf-8'))  # normalize
     missing_padding = len(data) % 4
     if missing_padding:
         data += b'='* (4 - missing_padding)
