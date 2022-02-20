314e3449bae76a6614f055a03f956a170247e050
Safa Safari <safasafari.ss@gmail.com>
2022-02-20 23:27:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
-import base64
-import asyncio
-import time
-import sys
-import json
 import re
+import sys
 import flag
+import json
+import time
+import base64
+import pproxy
+import urllib
+import asyncio
 from aiohttp import ClientSession
 from aiohttp.helpers import BasicAuth
 from aiohttp_socks import ProxyConnector
-import ssl
-import urllib3
-import urllib
-urllib3.disable_warnings()
-ssl._create_default_https_context = ssl._create_unverified_context
 
 result = {}
 
-async def run(cmd, ret = False):
-    print(cmd)
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-    if ret:
-        stdout, stderr = await proc.communicate()
-        if proc.returncode == 0:
-            return stdout
-        else:
-            return False
 
 def decode_base64(data, altchars=b'+/'):
     """Decode base64, padding being optional.
@@ -36,47 +21,44 @@ def decode_base64(data, altchars=b'+/'):
     :returns: The decoded byte string.
 
     """
-    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data.encode('utf-8'))  # normalize
+    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'',
+                  data.encode('utf-8'))  # normalize
     missing_padding = len(data) % 4
     if missing_padding:
-        data += b'='* (4 - missing_padding)
+        data += b'=' * (4 - missing_padding)
     return base64.b64decode(data, altchars)
 
-async def get(proxy, ip):
-    proxy = 'socks5://{}'.format(proxy)
-    connector = ProxyConnector.from_url(proxy)
-    try:
-        async with ClientSession(connector=connector) as session:
-            start = time.perf_counter()
-            async with session.get('http://www.google.com/generate_204') as request:
-                p = time.perf_counter() - start
-                if request.status != 204:
-                    p = None
-            async with session.get('http://ipinfo.io/{}'.format(ip), headers={'User-Agent': 'curl/7.79.1'}) as r:
-                result = await r.json()
-                location = '{} {} - {} - {} - {}'.format(flag.flag(result['country']), result['country'], result['region'], result['city'], result['org'])
-        return [p, location]
-    except:
-        return [None]
 
-
-async def send(send, proxy):
+async def send(send):
     try:
         sys.argv[2]
     except IndexError:
         return
-    proxy = 'socks5://{}'.format(proxy)
+    proxy = 'socks5://127.0.0.1:3993'
     connector = ProxyConnector.from_url(proxy, rdns=True)
     async with ClientSession(connector=connector) as session:
         async with session.post('https://api.telegram.org/bot{}/sendMessage'.format(sys.argv[2]), data={"chat_id": "@Proxy0110", "text": send, "parse_mode": "markdown", "disable_web_page_preview": True}, ssl=False):
             pass  # :)
 
 
-async def ping(ip, port, enc, password, tag, n):
-    await run('ss-local -s {} -p {} -l {} -k {} -m {}'.format(ip, port, n, password, enc))
-    await asyncio.sleep(2)
-    p = await get("127.0.0.1:{}".format(n), ip)
-    return p
+async def ping(ss):
+    ip, port, enc, password, tag = await parse_ss(ss)
+    start = time.perf_counter()
+    conn = pproxy.Connection(
+        'ss://{}:{}@{}:{}'.format(enc, password, ip, port))
+    reader, writer = await conn.tcp_connect('google.com', 80)
+    writer.write(b'GET /generate_204 HTTP/1.1\r\n\r\n')
+    data = await reader.read(1024*16)
+    if not 'HTTP/1.1 204 No Content' in data.decode('utf-8'):
+        return False
+    p = time.perf_counter() - start
+    reader, writer = await conn.tcp_connect('ipinfo.io', 80)
+    writer.write(b'GET / HTTP/1.1\r\nHost: ipinfo.io\r\n\r\n')
+    data = await reader.read(1024*32)
+    result = json.loads(data.decode('utf-8').split('\r\n\r\n')[1])
+    location = '{} {} - {} - {} - {}'.format(flag.flag(
+        result['country']), result['country'], result['region'], result['city'], result['org'])
+    return [p, location]
 
 
 async def github(api, method, data={}):
@@ -92,6 +74,7 @@ async def github(api, method, data={}):
 async def upload_github(file):
     with open(file, 'r') as f:
         await github('repos/SafaSafari/SAFA_SS/contents/{}'.format(file), 'PUT', {'message': 'UPDATE', 'content': base64.b64encode(f.read().encode('utf-8')).decode('utf-8'), 'sha': json.loads(await github('repos/SafaSafari/SAFA_SS/contents/{}'.format(file), 'GET'))['sha']})
+
 
 async def parse_ss(ss):
     if ss[0:5] != "ss://":
@@ -111,15 +94,17 @@ async def parse_ss(ss):
     password = p2[1]
     return [ip, port, enc, password, urllib.parse.quote(urllib.parse.unquote(tag))]
 
+
 async def main(n, ss):
     global result
     ss = ss.strip()
     parse = await parse_ss(ss)
     if parse:
-        p = await ping(*parse, n)
+        p = await ping(ss)
         if p[0] != None:
             p[0] = (p[0] * 100).__round__()
-            result[ss + ("#" if '#' not in ss else '') + urllib.parse.quote(p[1] + "@Proxy0110")] = p[0]
+            result[ss.split('#')[0] + "#" +
+                   urllib.parse.quote(p[1] + "@Proxy0110")] = p[0]
 
 
 async def gather():
@@ -130,21 +115,23 @@ async def gather():
     n = 30
     chunk = [source[i:i + n] for i in range(0, len(source), n)]
     for proxies in chunk:
-        await asyncio.gather(*[main(n+3333, proxies[n]) for n in range(0, len(proxies) - 1)])
-        await run('pkill -f "ss-local.*l 33"')
-        print(str(source.index(proxies[0])) + '/' + str(len(source)), end="\r")
-        await asyncio.sleep(3)
+        await asyncio.gather(*[main(proxies[n]) for n in range(0, len(proxies) - 1)])
+
     sort = {k: v for k, v in sorted(
         result.items(), key=lambda item: item[1])}
+
     leaf = []
-    for proxy in sort:
-        leaf.append("SS=ss,{},{},encrypt-method={},password={}#{}".format(*(await parse_ss(proxy))))
-    text = "\n".join("`{}`\nPing:{}\n".format(server, str(ping)) for server, ping in list(sort.items())[:10])
     sslocal = []
     for proxy in sort:
-        sslocal.append("ss-local -s {} -p {} -l 3993 -m {} -k {} # {}".format(*(await parse_ss(proxy))))
-    ip, port, enc, password, tag = await parse_ss(list(sort.items())[0][0])
-    await run('ss-local -s {} -p {} -l {} -k {} -m {}'.format(ip, port, 9999, password, enc))
+        parse = await parse_ss(proxy)
+        leaf.append(
+            "SS=ss,{},{},encrypt-method={},password={}#{}".format(*parse))
+        sslocal.append(
+            "ss-local -s {} -p {} -l 3993 -m {} -k {} # {}".format(*parse))
+
+    text = "\n".join("`{}`\nPing:{}\n".format(server, str(ping))
+                     for server, ping in list(sort.items())[:10])
+
     with open('ss.txt', 'w+') as f:
         f.write("\n".join(sort))
 
@@ -157,11 +144,12 @@ async def gather():
     with open("SUBSCRIBE", "w+") as f:
         f.write(base64.b64encode(b"\n".join(server.encode('utf-8')
                 for server, ping in list(sort.items())[:10])).decode('utf-8'))
+
     await upload_github('SUBSCRIBE')
     await upload_github('ss.txt')
     await upload_github('leaf.txt')
     await upload_github('ss-local.txt')
-    await send(text, '127.0.0.1:9999')
+    await send(text)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(gather())
